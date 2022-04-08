98c5802371093b48803e26052179b1864d5610a7
AJuskys <81487998+AJuskys@users.noreply.github.com>
2022-04-08 19:17:16 +0100

    Adding - Import Cheat

--- 18. Async JavaScript and HTTP Requests/6.3 Project; Film Finder/public/script.js
+++ 18. Async JavaScript and HTTP Requests/6.3 Project; Film Finder/public/script.js
@@ -49,5 +49,17 @@ const showRandomMovie = () => {
 
 };
 
+// Populate dropdown menu with all the available genres
+const populateGenreDropdown = (genres) => {
+  const select = document.getElementById('genres')
+
+  for (const genre of genres) {
+      let option = document.createElement("option");
+      option.value = genre.id;
+      option.text = genre.name;
+      select.appendChild(option);
+  }
+};
+
 getGenres().then(populateGenreDropdown);
 playBtn.onclick = showRandomMovie;
